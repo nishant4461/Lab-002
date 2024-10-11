09b140c258a21be414b819178af002bd5a98f5c4
NISHANT <hooda2004nishant@gmail.com>
2024-10-10 23:08:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lab_02/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lab_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch sw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgView = findViewById(R.id.imageView);
+        sw = findViewById(R.id.spin_switch);
+
+
+        sw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            }
+            else
+            {
+
+                imgView.clearAnimation();
+
+            }
+
+        });
+    }
+
+
+}
